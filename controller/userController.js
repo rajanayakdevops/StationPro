ec19bb1a6a546d53e67d02e91bee3a6c8d1d434c
rajanayakdevops <rajanayak.devops@gmail.com>
2024-12-30 09:21:07 +0530

    this code is not working updating the code in the next commit

--- controller/userController.js
+++ controller/userController.js
@@ -21,235 +21,45 @@ exports.postShowTrainDetails = (req,res)=>{
   const to = to_station;
   const date = travel_date;
 
-  TrainAvailableModel.fetchTrainDetails(from, to, date, (traindetails) => {
+  let trainDetailsData = [];  
 
-    if (!traindetails || traindetails.length === 0) {
-      return res.render("showTrainDetails", {
-        traindetails: [],
-        errorMessage: "No trains available for the selected route and date.",
-        PageTitle: 'trainSearchResult',
-      });
+// Store train details
+async function storeTrainData(from_station, to_station, travel_date) {
+    const data = await TrainAvailableModel.fetchTrainDetails(from_station, to_station, travel_date);
+    
+    if (Object.keys(data).length > 0) {
+        trainDetailsData = data;
+        console.log("Data stored successfully:");
+    } else {
+        console.log("No data to store.");
     }
-
-
-
-   const sortedDetailsDataPath = path.join(rootDir,'Data','sortedDetails');
-
-   const sortedDetails = traindetails.data.map(train => {
-      // Create a new object with only the relevant keys
-      return {
-        train_number: train.train_number,
-        train_name: train.train_name,
-        train_number: train.train_number,
-        train_type: train.train_type,
-        from: train.from,
-        from_std: train.from_std,
-        to: train.to,
-        to_sta: train.to_sta,
-        train_date: train.train_date
-    
-      };
-    });
-
-    fs.writeFileSync(sortedDetailsDataPath,JSON.stringify(sortedDetails));
-
-    const trainNumbers = sortedDetails.map(element =>{
-      return { 
-        train_number: element.train_number
-      }
-    });
-    
-
-    console.log(trainNumbers);
-
-    const seatClassAvailable = async () => {
-      const seatDetailsArray = await Promise.all(
-        trainNumbers.map(async (element) => {
-          return new Promise((resolve, reject) => {
-            // currently the function is providing the details of a single train but in actual api call it will 
-            // return the details of the train number that will be passed
-            TrainAvailableModel.fetchTrainAvailClasses(element, (seatDetails) => {
-              if (!seatDetails || seatDetails.data === 0) {
-                console.log("empty seatDetails  returning empty object");
-                resolve({});  
-              } else {
-                resolve({
-                  train_number: seatDetails.data.trainNumber,
-                  train_name: seatDetails.data.trainName,
-                  class: seatDetails.data.class
-                });
-              }
-            });
-          });
-        })
-      );
-    // for now the this seatDetaisArray consist of single trian details even after passing multiple train number
-    // contain duplicate element 
-      return seatDetailsArray;
-    };
-
-    const seatDetailsDataPath = path.join(rootDir,'Data','seatDetailsDataPath');
-
-    seatClassAvailable().then((seatDetailsArray) => {
-      // console.log(JSON.stringify(seatDetailsArray)); 
-  
-      //  here in this duplicate train details is stored  
-      fs.writeFileSync(seatDetailsDataPath,JSON.stringify(seatDetailsArray));
-      
-    }).catch((error) => {
-      console.error("Error fetching seat class details:", error);
-    });
-    
-    ////////////////////////
-
-    // not in use 
-    // let seatClassArray = [];
-    // const finalTrainDetails = path.join(rootDir,'Data','finalTrainDetails');
-
-
-    
-// TrainAvailableModel.fetchSeatDetailsData((seatDetails) => {
-//   Promise.all(
-//     seatDetails.map(async (train) => {
-//       // Create a new object with only the relevant keys
-//       return {
-//         train_number: train.train_number,
-//         class: train.class,
-//       };
-//     })
-//   )
-//     .then((resolvedArray) => {
-//       seatClassArray = resolvedArray;
-      
-//       fs.writeFileSync(finalTrainDetails,JSON.stringify(seatClassArray));
-      
-//     })
-//     .catch((error) => {
-//       console.error("Error processing seat details:", error);
-//     });
-
-    
-// });
-
-
-
-function fetchSeatDetails(fromStationCode,toStationCode,dateOfJourney,callback){
-  const result = {};
-  let pendingCalls = 0;
-  // this seatDetailsPath contains duplicate elements so function call is made to the same train number 
-  fs.readFile(seatDetailsDataPath, 'utf8', (error, data) => {
-    if (error) {
-        console.log("Error while reading the seatDetails:", error.message);
-        return callback(error);
-    }
-  
-    try {
-        // Parse the JSON data
-        const seatDetails = JSON.parse(data);
-  
-        // Check if the parsed data is an array
-        if (Array.isArray(seatDetails)) {
-  
-          for (const train of seatDetails) {
-            const { train_number, class: seatClasses } = train;
-
-            if (!result[train_number]) {
-              result[train_number] = [];
-            }
-    
-            // For each class of the train, fetch seat availability
-            for (const seatClass of seatClasses) {
-
-              pendingCalls++;
-
-              TrainAvailableModel.fetchSeatAvailable(seatClass.value,fromStationCode,toStationCode,train_number,dateOfJourney,(seatAvailable)=>{
-                
-                // in this the result is having the details of a single train number with corresonding seat availabe 
-                result[train_number].push(seatAvailable);
-
-                pendingCalls--;
-
-                if (pendingCalls === 0) {
-                  console.log("All API calls completed:");
-                  return callback(null,result);
-                }
-                
-              })
-            }
-          }
-  
-  
-  
-  
-            
-        } else {
-          callback(new Error("Data is not an array"));
-        }
-    } catch (parseError) {
-      callback(parseError);
-    }
-  });
-  
 }
 
-let trainJourneyDetails = []; // Initialize an empty array
+// Get stored train details
+function getTrainData() {
+    if (!trainDetailsData || trainDetailsData.length === 0) {
+        console.log("array is empty ");
+        return res.render("showTrainDetails", {
+          traindetails: [],
+          errorMessage: "No trains available for the selected route and date.",
+          PageTitle: 'trainSearchResult',
+        });
+      }
+      console.log("passing the train details ");
+      return res.render("showTrainDetails", {
+        traindetails: trainDetailsData,
+        errorMessage: "",
+        PageTitle: 'trainSearchResult',
+      });
+}
 
-fetchSeatDetails(from, to, date, (error, details_seat) => {
-  if (error) {
-    console.error("Error fetching seat details:", error);
-    return;
-  }
-
- 
-  trainJourneyDetails = details_seat;
-
-  console.log(trainJourneyDetails);
-
- return res.render("showTrainDetails", {
-    traindetails: sortedDetails,
-    trainJourneyDetails: trainJourneyDetails,
-    errorMessage: "",
-    PageTitle: 'trainSearchResult',
-  });
-
-
-  
-  
- 
-
-  
-});
+// Example usage
+(async () => {
+    await storeTrainData(from,to,date);  // Store data
+    getTrainData();  // get train detail 
+})();
 
 
-
-      
-
-          
-
-
-      
-
-
-
-
-
-
-
-
-
-
-
-
-  
-    
-    // res.render("showTrainDetails", {
-    //   traindetails: sortedDetails,
-    //   errorMessage: "",
-    //   PageTitle: 'trainSearchResult',
-    // });
-  });
-
-  
 
  
   
